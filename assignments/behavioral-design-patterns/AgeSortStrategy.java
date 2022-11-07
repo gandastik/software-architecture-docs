d1736b7174aaeb10c4f7b5d88d18e9292e70b3b9
gandastik <gandastik@gmail.com>
2022-11-07 11:04:59 +0700

    ✨ implemented strategy pattern

--- assignments/behavioral-design-patterns/AgeSortStrategy.java
+++ assignments/behavioral-design-patterns/AgeSortStrategy.java
@@ -8,7 +8,10 @@ public class AgeSortStrategy implements SortStrategy {
     public void customSort(List<Person> people) {
         // YOU ARE NOT ALLOWED TO MODIFY THE CODE ABOVE THIS COMMENT !!
         // YOU MAY ADD UP TO 5 LINES OF CODE BELOW THIS COMMENT !! A LINE OF CODE MAY CONTAIN UP TO ONE SEMI-COLON !!
-
+        people.sort(new Comparator<Person>() {
+            public int compare(Person a, Person b)  {
+                return a.getAge() - b.getAge();
+        }});
     }
 
 }
